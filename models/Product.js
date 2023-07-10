2d2e5a5d3afdabd49e962b5f9d76160445e6c72c
Yuga <Trev@brennberry.com>
2023-07-10 15:13:21 -0400

    fixed maybe

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,10 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class ProductTag extends Model {}
 
-// set up fields and rules for Product model
-Product.init(
+ProductTag.init(
   {
     // define columns
     id: {
@@ -16,31 +13,18 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL(10,2),
-      allowNull: false,
-      validate: {
-        isDecimal: true,
-      },
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true,
-      },
-    },
-    category_id: {
+    product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category',
-        key: 'id',
-        unique: false,
+        model: 'product',
+        key: 'id'
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
       },
     },
   },
@@ -49,8 +33,8 @@ Product.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: 'product_tag',
   }
 );
 
-module.exports = Product;
+module.exports = ProductTag;
