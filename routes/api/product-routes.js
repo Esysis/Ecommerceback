fd9758b346477c4288408b0bafdbd0ac760763a8
Yuga <Trev@brennberry.com>
2023-07-10 20:57:30 -0400

    Transferred and working woohoo

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../../models');
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
@@ -7,12 +7,31 @@ const { Product, Category, Tag, ProductTag } = require('../../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, 
+    {
+      model: Tag,
+      through: ProductTag,
+    }]
+  }) .then(allProducts => res.json(allProducts))
+  .catch(err => {console.log(err); res.status(500).json(err)})
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Category, 
+    {
+      model: Tag,
+      through: ProductTag,
+    }]
+  }) .then(ProductTag => res.json(ProductTag))
+  .catch(err => res.status(400).json(err))
 });
 
 // create new product
@@ -94,6 +113,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(deletedProduct => res.json(deletedProduct))
+  .catch(err => res.status(400).json(err))
 });
 
 module.exports = router;
